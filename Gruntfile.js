efd0b74fb7e3bb96882f47fe90f7c62f064f51ce
Daniel Stern <daniel@danielstern.ca>
2014-11-01 16:03:48 -0700

    madepowergood

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function(grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 9000,
+                port: process.env.PORT || 3000,
                 open: true,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
