df23d055461b1302b6994cebe88a9195f6a5f208
Daniel Stern <daniel@danielstern.ca>
2014-11-01 17:18:15 -0700

    fixed-routes

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -7,22 +7,10 @@ angular.module("DemoApp",['ui.router'])
 	$stateProvider
 	.state('home', {
 		url: '/',
-		views:{
-			banner:{
-    			templateUrl:'partials/main-banner.html'
-			},
-			main:{
-    			templateUrl:'partials/main.html'
-			}
-		}
-	    
+    	templateUrl:'partials/main.html'
 	})
 	.state('usage',{
 		url: '/usage',
-		views:{
-			main:{
-    			templateUrl:'partials/usage.html'
-			}
-		}
+    	templateUrl:'partials/usage.html'
 	})
 })
